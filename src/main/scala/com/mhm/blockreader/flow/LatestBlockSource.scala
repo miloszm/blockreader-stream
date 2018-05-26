e21925a9a1b50d6df524dd43fb8b880228e79496
miloszm <milosz.muszynski@digital.hmrc.gov.uk>
2018-05-26 14:28:36 +0100

    added filtering of only new blocks

--- src/main/scala/com/mhm/blockreader/flow/LatestBlockSource.scala
+++ src/main/scala/com/mhm/blockreader/flow/LatestBlockSource.scala
@@ -1,5 +1,7 @@
 package com.mhm.blockreader.flow
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
@@ -14,12 +16,14 @@ import scala.concurrent.duration._
 import scala.util.Try
 
 object LatestBlockSource {
+  val VOID_BLOCK = -1
+  val lastSeenBlock = new AtomicInteger(VOID_BLOCK)
   def create(implicit as: ActorSystem, am: ActorMaterializer): Source[LatestBlock, NotUsed] = Source.fromGraph(GraphDSL.create() { implicit builder =>
     import GraphDSL.Implicits._
 
     case class NotUsedRequest(x: Int)
 
-    val source = Source.tick[Int](1.seconds, 2.seconds, 1)
+    val source = Source.tick[Int](1.seconds, 30.seconds, 1)
     val responseFlow: Flow[(Try[HttpResponse], NotUsedRequest), LatestBlock, NotUsed] = Flow[(Try[HttpResponse],NotUsedRequest)].mapAsync(5) {
       case (resultTry: Try[HttpResponse], _: NotUsedRequest) => resultTry.map {
         case httpResponse @ HttpResponse(StatusCodes.OK, _, _, _) =>
@@ -27,10 +31,20 @@ object LatestBlockSource {
         case _ => throw new IllegalStateException()
       }.get
     }
+    val filter: Flow[LatestBlock, LatestBlock, NotUsed] = {
+      Flow[LatestBlock].map { latestBlock =>
+        if (lastSeenBlock.get() != latestBlock.height){
+          lastSeenBlock.set(latestBlock.height)
+          latestBlock
+        } else {
+          LatestBlock(VOID_BLOCK)
+        }
+      }.filter(_.height != VOID_BLOCK)
+    }
     val poller = Http().cachedHostConnectionPoolHttps[NotUsedRequest]("blockchain.info")
 
     val latestBlocks = source.map(i => (HttpRequest(uri = "https://blockchain.info/latestblock"), NotUsedRequest(i))).via(poller)
-    val stream = latestBlocks ~> responseFlow
+    val stream = latestBlocks ~> responseFlow ~> filter
 
     SourceShape(stream.outlet)
   })
@@ -43,5 +57,5 @@ object SourceRun extends App {
 
   val sink = Sink.foreach(println)
   val f = LatestBlockSource.create.toMat(sink)(Keep.right).run()
-  Await.result[Done](f, 1.minute)
+  Await.result[Done](f, 20.minutes)
 }
