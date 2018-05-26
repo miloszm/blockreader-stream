96037baa801bcbad56030069daeab35c559f5d85
miloszm <milosz.muszynski@digital.hmrc.gov.uk>
2018-05-26 14:08:52 +0100

    working tick of latest block

--- src/main/scala/com/mhm/blockreader/flow/LatestBlockSource.scala
+++ src/main/scala/com/mhm/blockreader/flow/LatestBlockSource.scala
@@ -1,27 +1,35 @@
 package com.mhm.blockreader.flow
 
-import akka.NotUsed
+import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCodes}
+import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.{ActorMaterializer, SourceShape}
-import akka.stream.scaladsl.{Flow, GraphDSL, Sink, Source}
+import akka.stream.scaladsl.{Flow, GraphDSL, Keep, Sink, Source}
 import com.mhm.blockreader.model.LatestBlock
 
-import scala.util.{Failure, Success, Try}
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.util.Try
 
 object LatestBlockSource {
   def create(implicit as: ActorSystem, am: ActorMaterializer): Source[LatestBlock, NotUsed] = Source.fromGraph(GraphDSL.create() { implicit builder =>
     import GraphDSL.Implicits._
 
-    val source = Source(1 to 5)
-    val responseFlow = Flow[(Try[HttpResponse],LatestBlock)].map{
-      case (Success(response), _) => LatestBlock(response.toString().length)
-      case (Failure(e), lb) => lb
-    }
-    val poller = Http().cachedHostConnectionPoolHttps[LatestBlock]("blockchain.info")
+    case class NotUsedRequest(x: Int)
 
-    val latestBlocks = source.map(i => (HttpRequest(uri = "https://blockchain.info/latestblock"), LatestBlock(i))).via(poller)
+    val source = Source.tick[Int](1.seconds, 2.seconds, 1)
+    val responseFlow: Flow[(Try[HttpResponse], NotUsedRequest), LatestBlock, NotUsed] = Flow[(Try[HttpResponse],NotUsedRequest)].mapAsync(5) {
+      case (resultTry: Try[HttpResponse], _: NotUsedRequest) => resultTry.map {
+        case httpResponse @ HttpResponse(StatusCodes.OK, _, _, _) =>
+          Unmarshal(httpResponse).to[LatestBlock]
+        case _ => throw new IllegalStateException()
+      }.get
+    }
+    val poller = Http().cachedHostConnectionPoolHttps[NotUsedRequest]("blockchain.info")
+
+    val latestBlocks = source.map(i => (HttpRequest(uri = "https://blockchain.info/latestblock"), NotUsedRequest(i))).via(poller)
     val stream = latestBlocks ~> responseFlow
 
     SourceShape(stream.outlet)
@@ -34,5 +42,6 @@ object SourceRun extends App {
   implicit val actorMaterializer = ActorMaterializer()
 
   val sink = Sink.foreach(println)
-  LatestBlockSource.create.to(sink).run()
+  val f = LatestBlockSource.create.toMat(sink)(Keep.right).run()
+  Await.result[Done](f, 1.minute)
 }
